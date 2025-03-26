b271fe28b5ab077ab1053bc1305b87805dd1b39b
kkm <rudals9686@naver.com>
2025-03-26 10:20:43 +0900

    [ADD] init

---
+++ kkm/src/main/kotlin/kr/or/bok/kkm/KkmApplication.kt
@@ -0,0 +1,11 @@
+package kr.or.bok.kkm
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KkmApplication
+
+fun main(args: Array<String>) {
+	runApplication<KkmApplication>(*args)
+}
---
+++ kkm/src/test/kotlin/kr/or/bok/kkm/KkmApplicationTests.kt
@@ -0,0 +1,13 @@
+package kr.or.bok.kkm
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KkmApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
